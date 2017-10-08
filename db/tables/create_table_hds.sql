a0d6b3bebba4060559542a6b29e51b687358c67d
GabrielKrysa <gabrielkrysa@gmail.com>
2017-10-08 20:38:20 -0300

    Update create_table_hds.sql

--- db/tables/create_table_hds.sql
+++ db/tables/create_table_hds.sql
@@ -6,3 +6,11 @@ CREATE TABLE `hds` (
   PRIMARY KEY (`id_unico`),
   CONSTRAINT `hds_ibfk_1` FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table HDs(
+id_hd int primary key auto_increment,
+tamanho int not null,
+rpm int not null,
+conexão varchar(10) not null,
+fabricante varchar(20) not null,
+foreign key id(id_hd) references id_unico(id));
